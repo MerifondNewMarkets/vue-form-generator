1a1a95ecdc04f5dfc43ea30df260b6c9b0fa9820
Christopher Böhm <christopher.boehm@merifond-new-markets.de>
2017-01-25 11:06:01 +0100

    Add for vue-version 2.0

--- src/fields/fieldDateTimePicker.vue
+++ src/fields/fieldDateTimePicker.vue
@@ -1,8 +1,8 @@
 <template lang="jade">
 	.input-group.date
-		input.form-control(type="date", v-model="value", :autocomplete="schema.autocomplete", :disabled="disabled", :placeholder="schema.placeholder", :readonly="schema.readonly", :name="schema.inputName")
+		input.form-control(type="text", v-model="value", :autocomplete="schema.autocomplete", :disabled="disabled", :placeholder="schema.placeholder", :readonly="schema.readonly", :name="schema.inputName")
 		span.input-group-addon
-			
+			span.glyphicon.glyphicon-calendar
 </template>
 
 <script>
@@ -46,19 +46,22 @@
 
 		},
 
-		ready() {
-			if ($.fn.datetimepicker) {
-				$(this.$el).datetimepicker(defaults(this.schema.dateTimePickerOptions || {}, {
-					format: inputFormat
-				}));
-			}
-			else
-				console.warn("Bootstrap datetimepicker library is missing. Please download from https://eonasdan.github.io/bootstrap-datetimepicker/ and load the script and CSS in the HTML head section!");
+		mounted() {
+			this.$nextTick(function () {
+				if ($.fn.datetimepicker) {
+					$(this.$el).datetimepicker(defaults(this.schema.dateTimePickerOptions || {}, {
+						format: inputFormat
+					}));
+				} else {
+					console.warn("Bootstrap datetimepicker library is missing. Please download from https://eonasdan.github.io/bootstrap-datetimepicker/ and load the script and CSS in the HTML head section!");
+				}
+			});
 		},
 
 		beforeDestroy() {
-			if ($.fn.datetimepicker)
+			if ($.fn.datetimepicker){
 				$(this.$el).data("DateTimePicker").destroy();
+			}
 		}		
 	};
 </script>
