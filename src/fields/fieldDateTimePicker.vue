c219ecd68142fe953952fa626b41de89c22bd7a3
Christopher Böhm <christopher.boehm@merifond-new-markets.de>
2017-01-25 11:30:39 +0100

    Change fieldDateTimePicker for Bootstrap-4

--- src/fields/fieldDateTimePicker.vue
+++ src/fields/fieldDateTimePicker.vue
@@ -1,8 +1,6 @@
 <template lang="jade">
 	.input-group.date
-		input.form-control(type="text", v-model="value", :autocomplete="schema.autocomplete", :disabled="disabled", :placeholder="schema.placeholder", :readonly="schema.readonly", :name="schema.inputName")
-		span.input-group-addon
-			span.glyphicon.glyphicon-calendar
+		input.form-control(type="date", v-model="value", :autocomplete="schema.autocomplete", :disabled="disabled", :placeholder="schema.placeholder", :readonly="schema.readonly", :name="schema.inputName")
 </template>
 
 <script>
